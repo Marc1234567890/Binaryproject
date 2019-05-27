0893d957a66637429daf2bd50405022fd31de246
Marc Nimmerrichter <marc.nimmerrichter@rubicon.eu>
2019-05-27 13:53:55 +0200

    Projekt neu erstellt

--- exeproject/exeproject/Program.cs
+++ exeproject/exeproject/Program.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace exeprojectr
+namespace exeproject
 {
   class Program
   {
     static void Main (string[] args)
     {
-      Console.WriteLine ("Tesst");
+      Console.WriteLine ("Test");
     }
   }
 }
--- exeproject/exeproject/Properties/AssemblyInfo.cs
+++ exeproject/exeproject/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle ("exeprojectr")]
+[assembly: AssemblyTitle ("exeproject")]
 [assembly: AssemblyDescription ("")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
-[assembly: AssemblyProduct ("exeprojectr")]
+[assembly: AssemblyProduct ("exeproject")]
 [assembly: AssemblyCopyright ("Copyright ©  2019")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible (false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid ("0ca1ec83-c578-4377-8ce8-56f8e5e21ad8")]
+[assembly: Guid ("9ef440eb-6b9c-4657-b3f8-a75cf600c2cf")]
 
 // Version information for an assembly consists of the following four values:
 //
